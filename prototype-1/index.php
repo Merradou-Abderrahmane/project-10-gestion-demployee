90e7077eeeb2f2f05abb24ad9a4b12fca40e0315
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-02-17 17:00:23 +0100

    save

---
+++ prototype-1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prototype 2</title>
+</head>
+<body>
+    <div>
+        <a href="insert.php">Insert Data</a>
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+                <th>Action</th>
+            </tr>
+            <?
+            foreach($data as $value)
+            ?>
+        </table>
+    </div>
+</body>
+</html>
