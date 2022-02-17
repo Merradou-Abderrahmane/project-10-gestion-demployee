8b17d6d75656d2dd53389e4b2213fcc494e922c6
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-02-17 16:10:37 +0100

    second edit

--- prototype-0/insert.php
+++ prototype-0/insert.php
@@ -1,18 +1,19 @@
-<?php 
-    if ( !empty($_POST)) { 
-        // post values
-        $fname  = $_POST['fname'];
-        $lname  = $_POST['lname'];
-        $age    = $_POST['age'];
-        $gender = $_POST['gender'];
-		$person = array($fname, $lname, $age, $gender); 
-      
-		$file = file_get_contents('people.json');
-		$data = json_decode($file, true);
-		array_push($data, $person);
-		file_put_contents("people.json", json_encode($data));
-		header("Location: index.php");
-    }
+<?php
+if (!empty($_POST)){
+	// post Values
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$age = $_POST['age'];
+	$gender = $_POST['gender'];
+	// store them in an array
+	$person = array($firstName, $lastName, $age, $gender);
+
+	$content = file_get_contents("data.json");
+	$data = json_decode($content);
+	array_push($data, $person);
+	file_put_contents("data.json", json_encode($data));
+	header("location: index.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,41 +21,40 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Prototype-0</title>
 </head>
 <body>
-<div>
-        <div>
-		<div><h3>Create a User</h3>
-        <form method="POST" action="">
+	<div>
+		<h3>Create a new user :</h3>
+		<form method="POST">
 			<div>
-				<label for="inputFName">First Name</label>
-				<input type="text" required="required" id="inputFName" name="fname" placeholder="First Name">
+				<label for="inputFirstName">First Name</label>
+				<input type="text" required  name="firstName" placeholder="First Name">
 			</div>
-			
+
 			<div>
-				<label for="inputLName">Last Name</label>
-				<input type="text" required="required" id="inputLName" name="lname" placeholder="Last Name">
+				<label for="inputLastName">Last Name</label>
+				<input type="text" required  name="lastName" placeholder="Last Name">
 			</div>
-			
+
 			<div>
 				<label for="inputAge">Age</label>
-				<input type="number" required="required" class="form-control" id="inputAge" name="age" placeholder="Age">
+				<input type="number" required  name="age" placeholder="Age">
 			</div>
-				<div class="form-group">
-					<label for="inputGender">Gender</label>
-					<select class="form-control" required="required" id="inputGender" name="gender" >
-						<option>Please Select</option>
-						<option value="Male">Male</option>
-						<option value="Female">Female</option>
-					</select>
-        		</div>
-			<div class="form-actions">
-					<button type="submit">Create</button>
-					<a href="index.php">Back</a>
+			<div>
+				<label for="inputGender">Gender</label>
+				<select name="gender" required  >
+					<option>Select</option>
+					<option value="Male">Male</option>
+					<option value="Female">Female</option>
+				</select>
+			</div>
+			<div>
+				<button type="submit">Create</button>
+				<a href="index.php">Form</a>
+			</div>
 			</div>
 		</form>
-        </div></div>        
-</div>
+	</div>
 </body>
 </html>
