853a017c506a91df4ea575b2fef5eb352a7416e9
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2022-02-27 20:42:06 +0100

    require once

--- prototypes/prototype-3/tests/test-connection.php
+++ prototypes/prototype-3/tests/test-connection.php
@@ -1,6 +1,6 @@
 <?php
 // test on public function , then return it private and continue ...
-include '../employee.php';
+require_once '../employee.php';
 $employee = new Employee();
 $employee->setFirstName("Merradou");
 echo $employee->getFirstName();
