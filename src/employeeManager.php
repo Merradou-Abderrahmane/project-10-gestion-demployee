dd2b56eb6459dec7847a2e59e7dcced4a761cbad
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-03-04 11:22:53 +0100

    get all Employees method done

--- src/employeeManager.php
+++ src/employeeManager.php
@@ -1,5 +1,5 @@
 <?php
-    include 'employee.php';
+    require_once 'employee.php';
 
     class EmployeeManager {
         public $Connection = null;
@@ -15,5 +15,29 @@
             }
             return $this->Connection;
         }
+
+        public function getAllEmployees(){
+            $sqlGetData = 'SELECT * FROM employee';
+            $result = mysqli_query($this->getConnection(), $sqlGetData);
+            $employeesList = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+            $employees = array();
+    
+            foreach($employeesList as $employeeList){
+                $employee = new Employee();
+                $employee->setId($employeeList['id']);
+                $employee->setRegistrationNumber($employeeList['registrationNumber']);
+                $employee->setFirstName($employeeList['firstName']);
+                $employee->setLastName($employeeList['lastName']);
+                $employee->setBirthDate($employeeList['birthDate']);
+                $employee->setDepartment($employeeList['department']);
+                $employee->setSalary($employeeList['salary']);
+                $employee->setOccupation($employeeList['occupation']);
+                $employee->setPhoto($employeeList['photo']);
+                array_push($employees, $employee);  
+            }
+            return $employees;
+        }
+
     }
 ?>
